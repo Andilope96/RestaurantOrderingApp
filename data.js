c7594b65d7cb34ee3ac847f291160f477e9093a9
Andreas Kühltau <andi.kuehltau@t-online.de>
2025-12-24 05:04:58 +0100

    added HTML and CSS

---
+++ data.js
@@ -0,0 +1,25 @@
+const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        pic: "./images/pizza.jpg"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        pic: "./images/burger.jpg",
+        id: 1
+    },
+        {
+        name: "Beer",
+        ingredients: ["grain, hops, yeast, water"],
+        price: 12,
+        pic: "./images/beer.jpg",
+        id: 2
+    }
+]
+
+export default menuArray
